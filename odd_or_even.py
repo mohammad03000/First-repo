4972654d35ea79e4c5dcb89b61545fb4e630ec6b
mohammad03000 <127683726+mohammad03000@users.noreply.github.com>
2023-03-12 17:32:24 -0400

    create odd or even checker

---
+++ odd_or_even.py
@@ -0,0 +1,12 @@
+number = input("enter a number: ")
+number = int(number)
+
+variable = number %2
+if variable == 0:
+    print('even')
+if variable == 1:
+    print('odd you loser')
+# if number == 10:
+#     print(number)
+# else:
+#     print("dumbass")S
